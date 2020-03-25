6b6e4ec6cb7d6562c39697483398f070c1ff4b25
sportokalidis <45517041+sportokalidis@users.noreply.github.com>
2020-03-26 01:12:34 +0200

    Update plot.py

--- producers-consumers/plot.py
+++ producers-consumers/plot.py
@@ -10,16 +10,18 @@ Q_8 = [2.0255166666667163e-05,  0.00013577778666657136, 0.0001750878600000713]
 Q_16 = [1.3513250000000171e-05,  1.3279460000002778e-05, 1.3443524166667666e-05]
 
 
-plt.plot(LOOPS, Q_1)
-plt.plot(LOOPS, Q_4)
-plt.plot(LOOPS, Q_8)
-plt.plot(LOOPS, Q_16)
+plt.plot(LOOPS, Q_1, label = 'Consumers = 1')
+plt.plot(LOOPS, Q_4, label = 'Consumers = 4')
+plt.plot(LOOPS, Q_8, label = 'Consumers = 8')
+plt.plot(LOOPS, Q_16, label = 'Consumers = 16')
 
 plt.scatter(LOOPS, Q_1)
 plt.scatter(LOOPS, Q_4)
 plt.scatter(LOOPS, Q_8)
 plt.scatter(LOOPS, Q_16)
 
+plt.axis([-3000, 309000, -0.002, 0.02])
+plt.legend( loc="upper left")
 plt.title("Queuesize = 1,000   P = 4")
 plt.ylabel('remaining time (s)')
 plt.xlabel('Number of LOOPS')
@@ -32,17 +34,18 @@ Q_8 = [4.382474999999712e-05,  0.0003473219750002663, 0.00039003586500030892]
 Q_16 = [1.4044000000000242e-05,  1.692705500000191e-05, 1.7588898333336624e-05]
 
 
-plt.plot(LOOPS, Q_1)
-plt.plot(LOOPS, Q_4)
-plt.plot(LOOPS, Q_8)
-plt.plot(LOOPS, Q_16)
+plt.plot(LOOPS, Q_1, label = 'Consumers = 1')
+plt.plot(LOOPS, Q_4, label = 'Consumers = 4')
+plt.plot(LOOPS, Q_8, label = 'Consumers = 8')
+plt.plot(LOOPS, Q_16, label = 'Consumers = 16')
 
 plt.scatter(LOOPS, Q_1)
 plt.scatter(LOOPS, Q_4)
 plt.scatter(LOOPS, Q_8)
 plt.scatter(LOOPS, Q_16)
 
-
+plt.axis([-3000, 309000, -0.002, 0.09])
+plt.legend( loc="upper left")
 plt.title("Queuesize = 5,000   P = 4")
 plt.ylabel('remaining time (s)')
 plt.xlabel('Number of LOOPS')
@@ -55,17 +58,18 @@ Q_8 = [2.4965249999999972e-05,  0.0005638388499991706, 0.0010919526749979364]
 Q_16 = [1.5728916666666966e-05,  1.7086808333336525e-05, 1.7534371666669344e-05]
 
 
-plt.plot(LOOPS, Q_1)
-plt.plot(LOOPS, Q_4)
-plt.plot(LOOPS, Q_8)
-plt.plot(LOOPS, Q_16)
+plt.plot(LOOPS, Q_1, label = 'Consumers = 1')
+plt.plot(LOOPS, Q_4, label = 'Consumers = 4')
+plt.plot(LOOPS, Q_8, label = 'Consumers = 8')
+plt.plot(LOOPS, Q_16, label = 'Consumers = 16')
 
 plt.scatter(LOOPS, Q_1)
 plt.scatter(LOOPS, Q_4)
 plt.scatter(LOOPS, Q_8)
 plt.scatter(LOOPS, Q_16)
 
-
+plt.axis([-3000, 309000, -0.01, 0.3])
+plt.legend( loc="upper left")
 plt.title("Queuesize = 20,000   P = 4")
 plt.ylabel('remaining time (s)')
 plt.xlabel('Number of LOOPS')
